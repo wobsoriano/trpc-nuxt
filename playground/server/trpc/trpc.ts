41e611711316a2081601c5a28be30c564e58ab3a
Robert Soriano <sorianorobertc@gmail.com>
2022-10-30 21:30:07 -0700

    add zod error formatting for playground

--- playground/server/trpc/trpc.ts
+++ playground/server/trpc/trpc.ts
@@ -1,9 +1,23 @@
 import { initTRPC } from '@trpc/server'
 import superjson from 'superjson'
+import { ZodError } from 'zod'
 import type { Context } from './context'
 
 const t = initTRPC.context<Context>().create({
   transformer: superjson,
+  errorFormatter({ shape, error }) {
+    return {
+      ...shape,
+      data: {
+        ...shape.data,
+        zodError:
+          error.code === 'BAD_REQUEST'
+          && error.cause instanceof ZodError
+            ? error.cause!.flatten()
+            : null,
+      },
+    }
+  },
 })
 
 /**
