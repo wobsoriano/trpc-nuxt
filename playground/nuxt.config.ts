0ee322c8a29ce7038b8ffe4d7e8bfc12c08710e1
Robert Soriano <sorianorobertc@gmail.com>
2022-10-30 17:35:23 -0700

    run dev playground with concurrently

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  runtimeConfig: {
-    baseURL: '',
-  },
-  typescript: {
-    strict: true,
-  },
+  modules: ['trpc-nuxt/module'],
 })
