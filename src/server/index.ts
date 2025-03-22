de1429e6cf1c618269c3625070c496036226427d
wobsoriano <sorianorobertc@gmail.com>
2025-03-22 12:18:40 -0700

    update deprecated message

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,6 @@ import { createTRPCNuxtHandler } from './createTRPCNuxtHandler'
 export { createTRPCNuxtHandler }
 
 /**
- * @deprecated Use createTRPCNuxtHandler instead.
+ * @deprecated Use createTRPCNuxtHandler instead. This will be removed in the next minor version.
  */
 export const createNuxtApiHandler = createTRPCNuxtHandler
