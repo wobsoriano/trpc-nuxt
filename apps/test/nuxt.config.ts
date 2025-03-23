cb24d278d4a6663ea1c6d17b3700757498de5c64
wobsoriano <sorianorobertc@gmail.com>
2025-03-23 14:46:43 -0700

    test: add e2e tests

---
+++ apps/test/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  build: {
+    transpile: ['trpc-nuxt']
+  },
+})
