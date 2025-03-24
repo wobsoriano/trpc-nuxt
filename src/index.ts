24c0775910d2de9196341713883044a81c2b0d72
wobsoriano <sorianorobertc@gmail.com>
2025-03-24 10:56:55 -0700

    chore: code split and deprecated direct import of server functions in root path

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export * from './server';
+import { createTRPCNuxtHandler as _createTRPCNuxtHandler, createNuxtApiHandler } from './server';
+
+/**
+ * @deprecated Use `createTRPCNuxtHandler` from `trpc-nuxt/server` instead. This will be removed in the next minor version.
+ */
+const createTRPCNuxtHandler = _createTRPCNuxtHandler;
+
+export {
+  createNuxtApiHandler,
+  createTRPCNuxtHandler
+}
--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts', 'src/client/index.ts', 'src/server/index.ts'],
   format: ['esm'],
-  splitting: false,
+  splitting: true,
   clean: true,
   external: ['nuxt', 'vue', /@trpc\/client/, /@trpc\/server/],
   dts: true,
