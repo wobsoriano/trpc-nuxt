254828de96367945075be8ddf5764e13916eb6d1
wobsoriano <sorianorobertc@gmail.com>
2025-04-06 10:17:07 -0700

    chore: type updates

--- src/server/toWebRequest.ts
+++ src/server/toWebRequest.ts
@@ -1,7 +1,7 @@
 import type { H3Event } from 'h3';
 import { incomingMessageToRequest } from '@trpc/server/adapters/node-http';
 
-export function toWebRequest(event: H3Event) {
+export function toWebRequest(event: H3Event): Request {
   // TODO: Prepare for h3 v2 https://github.com/unjs/h3/blob/main/MIGRATION.md#migration-guide-for-v1-to-v2
   if ('request' in event) {
     return event.request as Request;
