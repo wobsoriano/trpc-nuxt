d0dfce44634f680be9f4fb2b67aa936ecea88fe3
wobsoriano <sorianorobertc@gmail.com>
2025-04-12 12:10:33 -0700

    chore: extract util

--- src/server/toWebRequest.ts
+++ src/server/toWebRequest.ts
@@ -1,5 +1,5 @@
 import type { H3Event } from 'h3';
-import { toWebRequest as toWebRequestOriginal } from 'h3';
+import { getRequestURL, getRequestWebStream } from 'h3';
 
 export function toWebRequest(event: H3Event): Request {
   // TODO: Prepare for h3 v2 https://github.com/unjs/h3/blob/main/MIGRATION.md#migration-guide-for-v1-to-v2
@@ -8,4 +8,14 @@ export function toWebRequest(event: H3Event): Request {
   }
 
   return toWebRequestOriginal(event);
+}
+
+// Copied from https://github.com/unjs/h3/blob/89265ec4cc37f65f5bf903c2b993fde3e6e69048/src/utils/request.ts#L351
+function toWebRequestOriginal(event: H3Event) {
+  return event.web?.request || new Request(getRequestURL(event), {
+    duplex: 'half',
+    method: event.method,
+    headers: event.headers,
+    body: getRequestWebStream(event),
+  });
 }
