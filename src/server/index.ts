39c1524806bab9faaea5686df3944f79932c21f4
wobsoriano <sorianorobertc@gmail.com>
2025-10-24 11:18:24 -0700

    chore: remove deprecated createNuxtApiHandler

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,3 @@
 import { createTRPCNuxtHandler } from './create-trpc-nuxt-handler';
 
 export { createTRPCNuxtHandler };
-
-/**
- * @deprecated Use createTRPCNuxtHandler instead. This will be removed in the next minor version.
- */
-export const createNuxtApiHandler = createTRPCNuxtHandler;
